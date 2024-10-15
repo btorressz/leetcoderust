19d856a811a42df078de11f4b786da39b5dae3ad
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-10-14 18:35:53 -0700

    Update 2938.rs

--- 2938.rs
+++ 2938.rs
@@ -1,1 +1,17 @@
 
+impl Solution {
+    pub fn minimum_steps(s: String) -> i64 {
+        let mut steps = 0;
+        let mut count_black = 0;
+
+        for ch in s.chars() {
+            if ch == '1' {
+                count_black += 1;
+            } else {
+                steps += count_black;
+            }
+        }
+
+        steps
+    }
+}
