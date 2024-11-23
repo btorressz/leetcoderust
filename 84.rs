a93628c86ef4d57bbbf0024e0e339e2053edb491
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-11-23 13:14:58 -0800

    Create 84.rs

---
+++ 84.rs
@@ -0,0 +1,42 @@
+impl Solution {
+    #[allow(dead_code)]
+    pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+        let n = heights.len();
+        let mut left = vec![-1; n];
+        let mut right = vec![-1; n];
+        let mut stack: Vec<(usize, i32)> = Vec::new();
+        let mut ret = -1;
+
+        for (i, h) in heights.iter().enumerate() {
+            while !stack.is_empty() && stack.last().unwrap().1 >= *h {
+                stack.pop();
+            }
+            if stack.is_empty() {
+                left[i] = -1;
+            } else {
+                left[i] = stack.last().unwrap().0 as i32;
+            }
+            stack.push((i, *h));
+        }
+
+        stack.clear();
+
+        for (i, h) in heights.iter().enumerate().rev() {
+            while !stack.is_empty() && stack.last().unwrap().1 >= *h {
+                stack.pop();
+            }
+            if stack.is_empty() {
+                right[i] = n as i32;
+            } else {
+                right[i] = stack.last().unwrap().0 as i32;
+            }
+            stack.push((i, *h));
+        }
+
+        for (i, h) in heights.iter().enumerate() {
+            ret = std::cmp::max(ret, (right[i] - left[i] - 1) * *h);
+        }
+
+        ret
+    }
+}
