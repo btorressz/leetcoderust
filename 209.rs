8f8db08aeee808549688ee786fabccb752e057db
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-11-26 12:09:49 -0800

    Update 209.rs

--- 209.rs
+++ 209.rs
@@ -1,5 +1,5 @@
 //209. Minimum Size Subarray Sum
-//Solution 1
+//Solution 1 is using sliding window algorithm 
 impl Solution {
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
         let mut start = 0;
@@ -20,3 +20,34 @@ impl Solution {
         }
     }
 } //end of solution 1
+
+//Solution 2 
+//For Solution 2 I tried out Binary Search.
+impl Solution {
+    pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut prefix = vec![0; n + 1];
+        for i in 0..n {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        let mut min_length = i32::MAX;
+        for i in 0..n {
+            let target_sum = target + prefix[i];
+            if let Ok(j) = prefix.binary_search_by(|&x| x.cmp(&target_sum)) {
+                min_length = min_length.min((j - i) as i32);
+            } else if let Err(j) = prefix.binary_search(&target_sum) {
+                if j <= n {
+                    min_length = min_length.min((j - i) as i32);
+                }
+            }
+        }
+        if min_length == i32::MAX {
+            0
+        } else {
+            min_length
+        }
+    }
+} //end of solution 2 
+
+
+
