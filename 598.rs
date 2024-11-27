0b6b49ae9013fc2ed580cf2b4efb5f666f956a3c
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-11-26 16:08:09 -0800

    Update 598.rs

--- 598.rs
+++ 598.rs
@@ -1,3 +1,23 @@
+//598. Range Addition II
+//Successful ATTEMPT
+impl Solution {
+    pub fn max_count(m: i32, n: i32, ops: Vec<Vec<i32>>) -> i32 {
+        if ops.is_empty() {
+            return m * n;
+        }
+
+        let mut min_row = m;
+        let mut min_col = n;
+
+        for op in ops {
+            min_row = min_row.min(op[0]);
+            min_col = min_col.min(op[1]);
+        }
+
+        min_row * min_col
+    }
+}
+
 /* 
 //ATTEMPT ONE 
 impl Solution {
