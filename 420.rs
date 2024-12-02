db3c2f08efc1b216db7256f44ee820abe72d18d0
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-12-02 08:47:18 -0800

    Update 420.rs

--- 420.rs
+++ 420.rs
@@ -1,5 +1,94 @@
 //420. Strong Password Checker
+//One of the hardest leetcode problems I have ever done
+//Successful solution
 
+impl Solution {
+    pub fn strong_password_checker(password: String) -> i32 {
+        let n = password.len();
+        let mut missing_types = 3;
+         et mut lower = false;
+        let mut upper = false;
+        let mut digit = false;
+
+     
+        // Count missing character types
+        for ch in password.chars() {
+            if ch.is_lowercase() { lower = true; }
+            if ch.is_uppercase() { upper = true; }
+            if ch.is_digit(10) { digit = true; }
+        }
+        missing_types -= (lower as i32 + upper as i32 + digit as i32);
+
+        if n < 6 {
+            return std::cmp::max(6 - n as i32, missing_types);
+        }
+
+        if n <= 20 {
+            let mut replace = 0;
+            let mut cnt = 0;
+            let mut prev = '\0';
+
+            for curr in password.chars() {
+                if curr == prev {
+                    cnt += 1;
+                } else {
+                    replace += cnt / 3;
+                    cnt = 1;
+                    prev = curr;
+                }
+            }
+            replace += cnt / 3;
+            return std::cmp::max(replace as i32, missing_types);
+        }
+
+        let mut replace = 0;
+        let mut delete = n - 20; 
+        let mut remove2 = 0;
+        let mut cnt = 0;
+        let mut prev = '\0';
+
+        for curr in password.chars() {
+            if curr == prev {
+                cnt += 1;
+            } else {
+                if delete > 0 && cnt >= 3 {
+                    if cnt % 3 == 0 {
+                        delete -= 1;
+                        replace -= 1;
+                    } else if cnt % 3 == 1 {
+                        remove2 += 1;
+                    }
+                }
+                replace += cnt / 3;
+                cnt = 1;
+                prev = curr;
+            }
+        }
+
+        if delete > 0 && cnt >= 3 {
+            if cnt % 3 == 0 {
+                delete -= 1;
+                replace -= 1;
+            } else if cnt % 3 == 1 {
+                remove2 += 1;
+            }
+        }
+        replace += cnt / 3;
+
+        let use2 = std::cmp::min(std::cmp::min(replace, remove2), delete / 2);
+        replace -= use2;
+        delete -= use2 * 2;
+
+        let use3 = std::cmp::min(replace, delete / 3);
+        replace -= use3;
+        delete -= use3 * 3;
+
+        (n - 20) as i32 + std::cmp::max(replace as i32, missing_types)
+    }
+}
+
+
+//First 2 attempts 
 /*
 impl Solution {
     pub fn strong_password_checker(password: String) -> i32 {
