83050bbeeef138dd570d3dd7f74c49c05d4d1168
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-12-08 23:28:53 -0800

    Update 69.rs

--- 69.rs
+++ 69.rs
@@ -1,5 +1,34 @@
 //69. Sqrt(x)
+
+//ATTEMPT TWO: SUCCESSFUL ATTEMPT 
 impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {
+        if x < 0 {
+            return 0;
+        }
+        
+        let mut left = 0;
+        let mut right = x / 2 + 1;
+        
+        while left <= right {
+            let mid = (right - left) / 2 + left;
+            let mid_squared = mid as i64 * mid as i64;  
+            
+            if mid_squared == x as i64 {
+                return mid;
+            } else if mid_squared < x as i64 {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        right
+    }
+}
+
+//ATTEMPT ONE: WRONG ANSWER
+/* impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
         if x < 0 {
             return 0;
@@ -23,3 +52,4 @@ impl Solution {
         right
     }
 }
+*/
