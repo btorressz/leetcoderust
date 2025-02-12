23ec08d67e8673e9c8e796aa8a6130dfb0e9d49c
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-02-12 10:05:36 -0800

    Create 1800.rs

---
+++ 1800.rs
@@ -0,0 +1,19 @@
+//1800. Maximum Ascending Subarray Sum
+
+impl Solution {
+    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+        let mut max_sum = 0;
+        let mut current_sum = nums[0];
+
+        for i in 1..nums.len() {
+            if nums[i - 1] < nums[i] {
+                current_sum += nums[i];
+            } else {
+                max_sum = max_sum.max(current_sum);
+                current_sum = nums[i];
+            }
+        }
+
+        max_sum.max(current_sum)
+    }
+}
