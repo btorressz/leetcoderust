9b230f54e4554028e8206c658cded083868b9826
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-02-17 17:25:25 -0800

    Update 332.rs

--- 332.rs
+++ 332.rs
@@ -1,3 +1,5 @@
+//332. Reconstruct Itinerary
+
 //attempt one: wrong answer 
 /*use std::collections::{BTreeMap, BTreeSet, VecDeque};
 
@@ -30,3 +32,37 @@ impl Solution {
         itinerary
     }
 }*/
+
+//Attempt two: Successful 
+use std::collections::{BTreeMap, BinaryHeap, VecDeque};
+
+impl Solution {
+    pub fn find_itinerary(tickets: Vec<Vec<String>>) -> Vec<String> {
+        let mut flight_map: BTreeMap<String, BinaryHeap<std::cmp::Reverse<String>>> = BTreeMap::new();
+
+        // Build adjacency list with a min-heap to maintain lexical order
+        for ticket in tickets {
+            flight_map
+                .entry(ticket[0].clone())
+                .or_insert_with(BinaryHeap::new)
+                .push(std::cmp::Reverse(ticket[1].clone()));
+        }
+
+        let mut stack = VecDeque::new();
+        let mut itinerary = Vec::new();
+        stack.push_back("JFK".to_string());
+
+        while let Some(airport) = stack.back() {
+            if let Some(destinations) = flight_map.get_mut(airport) {
+                if let Some(std::cmp::Reverse(next_dest)) = destinations.pop() {
+                    stack.push_back(next_dest);
+                    continue;
+                }
+            }
+            itinerary.insert(0, stack.pop_back().unwrap());
+        }
+
+        itinerary
+    }
+}
+
