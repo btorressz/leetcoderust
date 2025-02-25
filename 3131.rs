bd6078c71c4c54bbc37b87044b9bd137da239d29
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-02-25 10:53:39 -0800

    Create 3131.rs

---
+++ 3131.rs
@@ -0,0 +1,7 @@
+//3131. Find the Integer Added to Array I
+
+impl Solution {
+    pub fn added_integer(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        nums2.iter().min().unwrap() - nums1.iter().min().unwrap()
+    }
+}
