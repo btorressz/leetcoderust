cc3363c48772d43e1d1210d2f2246da4afc4d864
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-02-24 17:16:01 -0800

    Update 2787.rs

--- 2787.rs
+++ 2787.rs
@@ -22,3 +22,35 @@
     }
 }
 */
+
+//Attempt two: correct use std::collections::HashMap;
+
+impl Solution {
+    pub fn number_of_ways(n: i32, x: i32) -> i32 {
+        const MOD: i32 = 1_000_000_007;
+        let mut memo = HashMap::new();
+
+        // recursive function with memoization
+        fn dfs(n: i32, i: i32, x: i32, memo: &mut HashMap<(i32, i32), i32>) -> i32 {
+            if n == 0 {
+                return 1;
+            }
+            if i.pow(x as u32) > n {
+                return 0;
+            }
+            if let Some(&res) = memo.get(&(n, i)) {
+                return res;
+            }
+
+            let inc = dfs(n - i.pow(x as u32), i + 1, x, memo) % MOD;
+            let exc = dfs(n, i + 1, x, memo) % MOD;
+
+            let res = (inc + exc) % MOD;
+            memo.insert((n, i), res);
+            res
+        }
+
+        dfs(n, 1, x, &mut memo)
+    }
+}
+
