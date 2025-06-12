3e9465086fe1f762012dfce88ec46220aab93ed3
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-06-12 15:45:52 -0700

    Update 3994.rs

--- 3994.rs
+++ 3994.rs
@@ -42,5 +42,36 @@
 
 
 
-
 */
+
+//Attempt two: Successful 
+impl Solution {
+    pub fn check_valid_cuts(_n: i32, rectangles: Vec<Vec<i32>>) -> bool {
+        fn can_cut(mut intervals: Vec<(i32, i32)>) -> bool {
+            intervals.sort();
+            let mut groups = 1;
+            let mut current_end = intervals[0].1;
+
+            for i in 1..intervals.len() {
+                let (start, end) = intervals[i];
+                if start >= current_end {
+                    groups += 1;
+                    current_end = end;
+                } else {
+                    current_end = current_end.max(end);
+                }
+
+                if groups >= 3 {
+                    return true;
+                }
+            }
+            false
+        }
+
+        let horizontal: Vec<(i32, i32)> = rectangles.iter().map(|r| (r[1], r[3])).collect();
+        let vertical: Vec<(i32, i32)> = rectangles.iter().map(|r| (r[0], r[2])).collect();
+
+        can_cut(horizontal) || can_cut(vertical)
+    }
+}
+
