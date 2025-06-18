882acfde6ef377a538e3dbf8e797dcbc4695d689
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-06-18 10:08:49 -0700

    Create 2966.rs

---
+++ 2966.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn divide_array(mut nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+        let n = nums.len();
+        let mut res = Vec::new();
+
+        nums.sort();
+
+        for i in (2..=n).step_by(3) {
+            let group = vec![nums[i - 2], nums[i - 1], nums[i]];
+            if group[2] - group[0] <= k {
+                res.push(group);
+            } else {
+                return Vec::new(); 
+            }
+        }
+
+        res
+    }
+}
