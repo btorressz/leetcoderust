06eea8289b4da28a184f596367e591d0feea823b
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2025-08-23 22:17:59 -0700

    Create 1046.rs

---
+++ 1046.rs
@@ -0,0 +1,19 @@
+//1046. Last Stone Weight
+
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+        let mut heap = BinaryHeap::from(stones);
+
+        while heap.len() > 1 {
+            let stone1 = heap.pop().unwrap();
+            let stone2 = heap.pop().unwrap();
+            if stone1 != stone2 {
+                heap.push((stone1 - stone2).abs());
+            }
+        }
+
+        heap.pop().unwrap_or(0)
+    }
+}
